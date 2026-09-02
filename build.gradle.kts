382b7ea5f629f7af81754117b8cdf5c70163ea3c
twfjcn <127495012+twfjcn@users.noreply.github.com>
2026-09-02 09:58:00 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+// Top-level build file
 buildscript {
     repositories {
         google()
@@ -6,13 +7,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
